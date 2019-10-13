87707a86ac091e389a1e2727ae7ac78e4573c08d
Hemin Johnson <achayan1992@gmail.com>
2019-10-13 22:26:02 +0200

    feat: add an endpoint that returns all the appointment data

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,12 @@
 const express = require("express");
 
+const loadData = require('../services/loadData')
+
 const router = new express.Router();
 
 router.get("/api/appointments", async (req, res) => {
-  res.send("test");
+  const appointmentData = loadData('appointmentData.json')
+  res.send(appointmentData);
 });
 
 module.exports = router;
